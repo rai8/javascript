e8a33f38e8df529ac3d09a55073424c03e0343f0
rai8 <hytonne@outlook.com>
2019-08-23 13:09:02 +0300

    completed project

--- Final-project/index.js
+++ Final-project/index.js
@@ -3,7 +3,8 @@ function set() {
   for (let index = 0; index < elements.length; index++) {
     let cssProperty = elements[index].getAttribute("id");
     let cssValue = elements[index].value;
-    console.log(cssProperty, cssValue);
+    let div = document.getElementById("modify");
+    div.style[cssProperty] = cssValue;
   }
 }
 document.getElementById("set").addEventListener("click", set);
