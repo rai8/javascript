ba0c8d96133c3f23ac56fb8bb73e9e786a1a2119
rai8 <hytonne@outlook.com>
2019-08-23 12:59:23 +0300

    get attribute vslue

--- Final-project/index.js
+++ Final-project/index.js
@@ -1,7 +1,9 @@
 function set() {
   let elements = document.getElementsByName("cssProperty");
   for (let index = 0; index < elements.length; index++) {
-    console.log(index);
+    let cssProperty = elements[index].getAttribute("id");
+    let cssValue = elements[index].value;
+    console.log(cssProperty, cssValue);
   }
 }
 document.getElementById("set").addEventListener("click", set);
